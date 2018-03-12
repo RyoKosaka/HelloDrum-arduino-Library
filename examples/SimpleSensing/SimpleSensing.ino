8cbca61f5f6539ddb88f15d7d022f2bf0b8bebfe
Ryo Kosaka <r.k1227@gmail.com>
2018-03-12 20:29:16 +0900

    Update SimpleSensing.ino

--- examples/SimpleSensing/SimpleSensing.ino
+++ examples/SimpleSensing/SimpleSensing.ino
@@ -17,17 +17,21 @@
 
 */
 
-/* EXAMPLE 1 - Simple Single Piezo Sensing
- 
+/* EXAMPLE 1 - Simple Piezo Sensing
+
+ With this sample code, you will make snare and tom using two piezo.
 
 */
 
 
 ///////////////////////////////   INITIAL VALUE   ///////////////////////////////////
 
-//{sensitivity, threshold1, threshold2, retrigger, note}
+//Determine the initial value.
+//By changing the number in this array you can set sensitivity, threshold and so on.
 
+//{sensitivity, threshold1, threshold2, retrigger, note}
 int SNARE[6] = {800, 20, 50, 10, 38};
+int TOM[6] = {800, 20, 50, 10, 50};
 
 /////////////////////////////////////////////////////////////////////////////////////
 
@@ -35,20 +39,39 @@ int SNARE[6] = {800, 20, 50, 10, 38};
 #include <hellodrum.h>
 MIDI_CREATE_DEFAULT_INSTANCE();
 
-HelloDrum snare(0);
+//Please name your piezo.
+//The piezo named snare is connected to the A0 pin and the piezo named tom is connected to the A1 pin.
+
+HelloDrum snare(0); 
+HelloDrum tom(1);
 
 void setup() {
+  //If you use Hairless MIDI, you have to comment out the next line.
+  //MIDI.begin(10);
+
+  //And uncomment the next two lines.
   MIDI.begin();
   Serial.begin(38400);
 }
 
 void loop() {
 
+  //Piezo sensing is done in this line. And it is returned as a velocity of 127 stages.
+  //For each piezo, one line is required.
   snare.singlePiezo(SNARE[0], SNARE[1], SNARE[2], SNARE[3]);
-
+  tom.singlePiezo(TOM[0], TOM[1], TOM[2], TOM[3]);
+  
+  
+  //MIDI signals are transmitted with this IF statement.
+  //For each piezo, one IF statement is required
   if (snare.hit == true) {
     MIDI.sendNoteOn(SNARE[4], snare.velocity, 10);  //(note, velocity, channel)
     MIDI.sendNoteOff(SNARE[4], 0, 10);
+  }
+  
+  else if (tom.hit == true) {
+    MIDI.sendNoteOn(TOM[4], tom.velocity, 10);  //(note, velocity, channel)
+    MIDI.sendNoteOff(TOM[4], 0, 10);
   }
 
 }
