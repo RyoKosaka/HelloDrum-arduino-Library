2374056b9ff144d3d3442a06717962eef83217db
Ryo Kosaka <r.k1227@gmail.com>
2018-03-12 19:38:13 +0900

    Add files via upload

---
+++ examples/SimpleSensing/SimpleSensing.ino
@@ -0,0 +1,55 @@
+/*
+  " HELLO DRUM LIBRARY" Ver.0.1
+
+  Created March 1, 2018
+  by Ryo Kosaka
+
+  https://open-e-drums.tumblr.com/
+*/
+
+/* NOTICE
+
+  You have to install some library.
+  1.MIDI : https://playground.arduino.cc/Main/MIDILibrary
+
+  If you use LCD,
+  2.LCD Bar Graph : https://playground.arduino.cc/Code/LcdBarGraph
+
+*/
+
+/* EXAMPLE 1 - Simple Single Piezo Sensing
+ 
+
+*/
+
+
+///////////////////////////////   INITIAL VALUE   ///////////////////////////////////
+
+//{sensitivity, threshold1, threshold2, retrigger, note}
+
+int SNARE[6] = {800, 20, 50, 10, 38};
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+#include <MIDI.h>
+#include <hellodrum.h>
+MIDI_CREATE_DEFAULT_INSTANCE();
+
+HelloDrum snare(0);
+
+void setup() {
+  MIDI.begin();
+  Serial.begin(38400);
+}
+
+void loop() {
+
+  snare.singlePiezo(SNARE[0], SNARE[1], SNARE[2], SNARE[3]);
+
+  if (snare.hit == true) {
+    MIDI.sendNoteOn(SNARE[4], snare.velocity, 10);  //(note, velocity, channel)
+    MIDI.sendNoteOff(SNARE[4], 0, 10);
+  }
+
+}
+
---
+++ hellodrum.h
@@ -0,0 +1,40 @@
+#ifndef HelloDrum_h
+#define HelloDrum_h
+
+class HelloDrum
+{
+  public:
+    HelloDrum(int pin1, int pin2);
+    HelloDrum(int pin1);
+
+    void HelloDrum::singlePiezo(int sens, int thre1, int thre2, int retri);
+
+    //void singlePiezo(int sens, int thre1, int thre2, int retri);
+    void HelloDrum::dualPiezo(int sens, int thre1, int thre2, int retri);
+    void HelloDrum::cymbal3zone(int sens, int thre1, int thre2, int retri);
+    void HelloDrum::cymbal2zone(int sens, int thre1, int thre2, int retri);
+    void HelloDrum::hihat(int sens, int thre1, int thre2, int retri);
+    void HelloDrum::hihatControl(int sens, int thre1, int thre2, int retri);
+
+    int velocity;
+    int note;
+    int exValue;
+    bool hit;
+    bool hit_rim;
+    bool hit_edge;
+    bool hit_cup;
+    bool choke;
+    bool flag;
+    bool chokeFlag;
+    unsigned long time_hit;
+    unsigned long time_end;
+    unsigned long time_choke;
+
+  private:
+    int pin_1;
+    int pin_2;
+    int piezoValue;
+    int sensorValue;
+};
+
+#endif
