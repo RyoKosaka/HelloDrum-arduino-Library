3a29f26f27ef798bb7edc821527803bfe9302065
Ryo Kosaka <r.k1227@gmail.com>
2018-05-20 03:07:57 +0900

    Update InitializeEEPROM.ino

--- examples/InitializeEEPROM/InitializeEEPROM.ino
+++ examples/InitializeEEPROM/InitializeEEPROM.ino
@@ -3,9 +3,8 @@
   EXAMPLE - Initialization of EEPROM
 
   With this sample code, you can initialize the EEPROM.
-  Please write this code only once, if you want to use setting mode with this library.
+  Please write this code befor making your e-drum, if you want to use setting mode with this library.
   When writing is completed, the initial value setting is completed.
-  So let's set the value using the "SensingWithLCD" sample.
   
   The values after initialization are as follows.
     
@@ -29,25 +28,42 @@ HelloDrum pad_2(2);
 HelloDrum pad_3(3);
 HelloDrum pad_4(4);
 HelloDrum pad_5(5);
+//if you use arduino MEGA, uncomment below lines.
+/*
 HelloDrum pad_6(6);
 HelloDrum pad_7(7);
 HelloDrum pad_8(8);
 HelloDrum pad_9(9);
+HelloDrum pad_10(10);
+HelloDrum pad_11(11);
+HelloDrum pad_12(12);
+HelloDrum pad_13(13);
+HelloDrum pad_14(14);
+HelloDrum pad_15(15);
+*/
 
 void setup() {
   
-  //Initialize 10 pads.
+  //Initialize
   pad_0.initMemory();
   pad_1.initMemory();
   pad_2.initMemory();
   pad_3.initMemory();
   pad_4.initMemory();
   pad_5.initMemory();
+  //if you use arduino MEGA, uncomment below lines.
+  /*
   pad_6.initMemory();
   pad_7.initMemory();
   pad_8.initMemory();
   pad_9.initMemory();
-  
+  pad_10.initMemory();
+  pad_11.initMemory();
+  pad_12.initMemory();
+  pad_13.initMemory();
+  pad_14.initMemory();
+  pad_15.initMemory();
+  */
 }
 
 void loop() {
